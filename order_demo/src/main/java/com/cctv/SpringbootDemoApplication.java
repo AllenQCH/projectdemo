789ceab1367cfe49d0fd98357e50dc634e5aa5fb
AllenQCH <1181303285@qq.com>
2023-12-17 21:52:15 +0800

    æcommit a project with nothing

---
+++ order_demo/src/main/java/com/cctv/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cctv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class,args);
+    }
+}
---
+++ order_demo/src/main/java/com/cctv/service/UwsAttachmentService.java
@@ -0,0 +1,15 @@
+//package com.cctv.service;
+//
+//import com.cctv.pojo.UwsAttachment;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UwsAttachmentService {
+//    @Autowired
+//    private UwsAttachmentMapper uwsAttachmentMapper;
+//
+//    public UwsAttachment selectByPrimaryKey(Long id) {
+//        return uwsAttachmentMapper.selectByPrimaryKey(id);
+//    }
+//}
